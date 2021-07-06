f1655b21a902f578824154dd0e849922301e6020
Ingrid Rozema <67099368+IngridRozema@users.noreply.github.com>
2021-07-06 10:29:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+<script language="JavaScript">
+    
+    let name='Ingrid';
+    console.log(name);
+
+    let x = 4 + 4;
+    console.log(x);
+
+    let a = 4;
+    let b = 4;
+    let c = a + b;
+    console.log(c);
+
+    let d = 90;
+    let e = 27;
+    let f = d - e;
+    console.log(f);
+
+    let A = 12;
+    let B = (8 + 2) * A;
+    console.log(B);
+
+    let X = 50;
+    let Y = (X / 4);
+    console.log(Y);
+
+    let numberOne = 50;
+    let numberTwo = 100;
+    let numberThree = (numberOne % numberTwo);
+    console.log(numberThree);
+
+    let age = "drieenveertig";
+    typeof age;
+
+</script>
